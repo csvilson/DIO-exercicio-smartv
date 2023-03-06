c750e7e1b7f7965f1cd20a1e60e0458642ac5d9c
csvilson <csvilson@gmail.com>
2023-03-06 11:25:53 -0300

    Inseridos comentários para documentação Java Doc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.dio.minhaSmartv","l":"Smartv"}];updateSearchResults();
--- src/com/dio/minhaSmartv/Smartv.java
+++ src/com/dio/minhaSmartv/Smartv.java
@@ -1,26 +1,38 @@
 
-/*DESAFIO: Criar controle de SmarTV com as seguintes características:
+package com.dio.minhaSmartv;
+
+/** 
+ *  <h2>Class SmarTV<h2>
+ 	Desafio da DIO - criar controle de SmarTV com as seguintes características:
 	>Ligar e desligar
 	>Aumentar ou diminuir o volume em +1 ou -1
 	>Mudar de canal de 1 em 1 ou inserindo um número específico
+	@autor Vilson Sousa
+	@version 1.0
+	@since 06/03/2023
 */
-package com.dio.minhaSmartv;
-
 public class Smartv {
 	
 	public boolean ligado = false;
 	public int volume = 25;
 	public int canal = 1;
 	
-	
+	/**
+	 * Método ligar possui a finalidade de ligar a TV
+	 */
 	public void ligar() {
 		this.ligado = true;
 	}
-	
+	/**
+	 * Método desligar possui a finalidade de desligar a TV
+	 */
 	public void desligar() {
 		this.ligado = false;
 	}
 	
+	/**
+	 * Método aumentarVolume possui a finalidade de aumentar o volume da TV
+	 */
 	public void aumentarVolume() {
 		volume++;
 		System.out.println("Aumentando volume para: " + volume);
@@ -31,6 +43,10 @@ public class Smartv {
 		System.out.println("Diminuindo volume para: " + volume);
 	}
 	
+	/**
+	 * Método novoCanal possui a finalidade de mudar de canal a partir de um número específico
+	 * @param outroCanal 
+	 */
 	public void novoCanal(int outroCanal) {
 		canal = outroCanal;
 	}
